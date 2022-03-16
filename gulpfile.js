b506769c95d595fc5d8e42f52f72872f51c97afe
riicky-hub <richardjosephace1960@gmail.com>
2022-03-16 15:32:14 -0300

    otimizados arquivos de libs e imgs

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
+// Requires
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
+const image = require('gulp-imagemin');
 
+// Funções
 function tarefaCSS(cb) {
-    return gulp.src('./vendor/**/css/*.css')
+    return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css')
         .pipe(concat('libs.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
@@ -13,12 +16,30 @@ function tarefaCSS(cb) {
 }
 
 function tarefaJS() {
-    return gulp.src('./vendor/**/*.js')
+    return gulp.src(['./node_modules/jquery/dist/jquery.js', './node_modules/bootstrap/dist/js/bootstrap.js'])
         .pipe(concat('libs.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/js'))
 }
 
+function tarefaIMG() {
+    return gulp.src('./img/**/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
+}
+
+// Comandos
 exports.styles = tarefaCSS;
 exports.scripts = tarefaJS;
+exports.images = tarefaIMG;
